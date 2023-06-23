3e940082186520d38e26eacb4a0912e9fa1304fa
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2023-06-23 16:04:00 +0330

    [TEST] Test Zero and random numbers

--- src/test/java/ir/sudoit/persianlangtoolbox/converters/PersianWordsConverterTest.java
+++ src/test/java/ir/sudoit/persianlangtoolbox/converters/PersianWordsConverterTest.java
@@ -8,28 +8,51 @@ class PersianWordsConverterTest {
 
 
     @Test
-    void testConvertToWords() {
-        // Test cases with various decimal numbers
-
-        // Test case: Integer number
+    void testConvertToWordsDefault() {
         String number1 = "12345.0";
         String expectedWords1 = "دوازده هزار سیصد چهل و پنج";
         assertEquals(expectedWords1, PersianWordsConverter.convertToWords(number1));
 
-        // Test case: Decimal number
         String number2 = "1234.567";
-        String expectedWords2 = "هزار و دویست سی و چهار و پانصد و شصت و هفت هزارم";
+        String expectedWords2 = "یک هزار دویست سی و چهار ممیز پانصد شست و هفت هزارم";
         assertEquals(expectedWords2, PersianWordsConverter.convertToWords(number2));
 
-        // Test case: Negative number
         String number3 = "-9876.54321";
-        String expectedWords3 = "منفی نه هزار هشتصد هفتاد و شش ممیز پنجاه و چهار صد و بیست و یک صدم";
+        String expectedWords3 = "منفی نه هزار هشتصد هفتاد و شش ممیز پنجاه و چهار هزار سیصد بیست و یک میلیاردم";
+        assertEquals(expectedWords3, PersianWordsConverter.convertToWords(number3));
+    }
+
+
+    @Test
+    void testConvertToWordsZero() {
+        String number1 = "0";
+        String expectedWords1 = "صفر";
+        assertEquals(expectedWords1, PersianWordsConverter.convertToWords(number1));
+
+        String number2 = "0d";
+        String expectedWords2 = "صفر";
+        assertEquals(expectedWords2, PersianWordsConverter.convertToWords(number2));
+
+        String number3 = "0D";
+        String expectedWords3 = "صفر";
         assertEquals(expectedWords3, PersianWordsConverter.convertToWords(number3));
 
-        // Test case: Zero
         String number4 = "0.0";
         String expectedWords4 = "صفر";
         assertEquals(expectedWords4, PersianWordsConverter.convertToWords(number4));
+
+        String number5 = "000";
+        String expectedWords5 = "صفر";
+        assertEquals(expectedWords4, PersianWordsConverter.convertToWords(number4));
+
+        String number6 = "0.0000";
+        String expectedWords6 = "صفر";
+        assertEquals(expectedWords4, PersianWordsConverter.convertToWords(number4));
+
+        String number7 = "0000.0";
+        String expectedWords7 = "صفر";
+        assertEquals(expectedWords4, PersianWordsConverter.convertToWords(number4));
     }
+
 
 }
